a552d45709d6f4b73a1a392fc53af400b3f5be8b
usha ahirwar <66367304+usha-12@users.noreply.github.com>
2023-01-13 16:06:39 +0530

    Added: Print Factorial 1 to 10 (Workshop 1 program )

--- src/com/brideglabz/arrayprogram/Factorial.java
+++ src/com/brideglabz/arrayprogram/Factorial.java
@@ -12,7 +12,6 @@ public class Factorial {
         }
 
     }
-
     public static void main(String[] args) {
         System.out.println("---Welcome to the Factorial Program----");
         calculateFactorial();
