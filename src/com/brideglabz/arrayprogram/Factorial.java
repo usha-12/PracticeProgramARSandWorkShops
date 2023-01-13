0b79c6cc7223df1f4acea424e94e3cb87a537017
usha-12 <usha12ahirwar@gmail.com>
2023-01-13 09:15:20 +0530

    Added: print Factorial from 1 to 10

--- src/com/brideglabz/arrayprogram/Factorial.java
+++ src/com/brideglabz/arrayprogram/Factorial.java
@@ -1,2 +1,21 @@
-package com.brideglabz.arrayprogram;public class Factorial {
+package com.brideglabz.arrayprogram;
+
+public class Factorial {
+    public static void calculateFactorial(){
+        int numberOfTime;
+        long factorial=1;
+        System.out.println();
+        for (numberOfTime =1; numberOfTime<=10; numberOfTime++){
+            factorial = factorial*numberOfTime;
+            System.out.println(" factorial " +numberOfTime+ " is =  "+factorial);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("---Welcome to the Factorial Program----");
+        calculateFactorial();
+
+    }
 }
